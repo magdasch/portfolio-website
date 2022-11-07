6f60b1379ca63a30d2fecbd6189266c0600f43f3
magdasch <magda.schindler@gmail.com>
2022-11-07 22:00:41 +0100

    Update scripts.js
    add js for toggle menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
-alert("Hello and welcome to my portfolio");
-console.log("Hello world")
+//alert("Hello and welcome to my portfolio");
+//console.log("Hello world")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+  }
+}
